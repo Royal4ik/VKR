d18a15e43b943a4be82f9b3afe8532750ccd7fac
Artem <art.sandin@yandex.ru>
2019-06-06 01:06:33 +0300

    Delete Main.py

--- src/Main.py
+++
@@ -1,6 +0,0 @@
-import GetOldTweets3 as got
-
-tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama whitehouse")\
-                                           .setMaxTweets(2)
-tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-print(tweet.text)
